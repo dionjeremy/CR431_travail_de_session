0adf772b0face459dbea1e53b4ea17744b8f053a
Jeremy Dion <dionjeremy7@gmail.com>
2025-10-18 20:05:01 -0400

    ajustements pour export des fonctions et refacto

--- devoir/devoir.psd1
+++ devoir/devoir.psd1
@@ -1,10 +1,10 @@
 @{
     RootModule        = 'devoir.psm1'
-    ModuleVersion     = '1.0.0'
+    ModuleVersion     = '1.0.1'
     Author            = 'Jeremy Dion'
     Description       = 'Devoir pour le cour CR451 module'
-    PowerShellVersion = '7.0'
-    FunctionsToExport = 'Get-Greeting'
-    CmdletsToExport   = @()
+    PowerShellVersion = '7.5'
+    FunctionsToExport = '*'
+    CmdletsToExport   = @('*')
     AliasesToExport   = @()
 }
--- devoir/devoir.psm1
+++ devoir/devoir.psm1
@@ -1,13 +1,23 @@
+<#commentaire pour la première fonction 
+ @params :
+ @return :
+#>
 function fonctionNumeroUn {
     Write-Host("appel fonction numéro 1");
 }
 
-
+<#commentaire pour la deuxième fonction 
+ @params :
+ @return :
+#>
 function fonctionNumeroDeux {
-     Write-Host("appel fonction numéro 1");
+     Write-Host("appel fonction numéro 2");
 }
 
-
+<#commentaire pour la troisième fonction 
+ @params :
+ @return :
+#>
 function fonctionNumeroTrois {
-     Write-Host("appel fonction numéro 1");
+     Write-Host("appel fonction numéro 2");
 }
